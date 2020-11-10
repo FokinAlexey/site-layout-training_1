b625314eee25272df07b2899f19c6a72cf13a86c
FokinAlexey <a.fokin1979@gmail.com>
2020-11-10 19:44:48 +0300

    add second section to layout

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-sdfsd
+$(function() {
+
+    $('.header-slider').slick({
+        arrows: false,
+        vertical: true,
+        dots: true, 
+        dotsClass: 'header-dots',
+        autoplay: 2000,
+        
+    });
+    
+});
