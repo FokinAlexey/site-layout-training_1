d1afc8f274104c4924001378708ea192a2ea89ad
FokinAlexey <a.fokin1979@gmail.com>
2020-11-13 15:09:10 +0300

    fin

--- js/main.js
+++ js/main.js
@@ -6,7 +6,10 @@ $(function() {
         dots: true, 
         dotsClass: 'header-dots',
         autoplay: 2000,
-        
     });
+
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').slideToggle();
+    })
     
 });
